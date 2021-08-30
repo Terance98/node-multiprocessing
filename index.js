a78471f084bc86ecfd8bb639d83d8a26a920fe47
Thomas Terance <terance98@minion.local>
2021-08-30 19:46:27 +0530

    normal isPrime check updates

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const { isPrime } = require("./isPrime");
 
 app.get("/isprime", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  const jsonResponse = isPrime(parseInt(req.query.number));
-  res.send(jsonResponse);
+  // This code will promisify a sync code. Don't know if this is safe in all cases
+  const jsonResponse = new Promise((resolve) =>
+    setTimeout(() => resolve(isPrime(parseInt(req.query.number))))
+  );
+  console.log(jsonResponse);
+  jsonResponse.then((resp) => res.send(resp));
+  // res.send({}); // Comment the above line and uncomment this line if the prime check needs to be executed asynchronously
 });
 
 app.listen(8081, () => console.log("Listening on 8081"));
